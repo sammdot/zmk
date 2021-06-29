4127696d147ba7cf64e4ec7cceab4a36fe172865
crides <zhuhaoqing@live.cn>
2021-06-29 15:38:44 -0500

    Use ILI9340 instead of LS0XX
    - Change back fg & bg style
    - change resolution
    - change hardware dts

--- app/src/display/widgets/wpm_status.c
+++ app/src/display/widgets/wpm_status.c
@@ -25,7 +25,7 @@ void wpm_status_init() {
 
     style_initialized = true;
     lv_style_init(&label_style);
-    lv_style_set_text_color(&label_style, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    lv_style_set_text_color(&label_style, LV_STATE_DEFAULT, LV_COLOR_BLACK);
     lv_style_set_text_font(&label_style, LV_STATE_DEFAULT, &lv_font_montserrat_12);
     lv_style_set_text_letter_space(&label_style, LV_STATE_DEFAULT, 1);
     lv_style_set_text_line_space(&label_style, LV_STATE_DEFAULT, 1);
